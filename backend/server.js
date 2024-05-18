cc3b3afb4804df33088ac094784a4eecf88c13b9
Himash Deemantha <136174066+himashdee@users.noreply.github.com>
2024-05-18 10:54:18 +0530

    Update server.js commit-02

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json())
 
-app.get('/', (req, res)=> res.send('hello world'));
+app.get('/', (req, res)=> res.send('hello world node 01'));
 
 
 app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`))
